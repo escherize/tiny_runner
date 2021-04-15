3d96b26e471f44f1240ea81c50d835f059f42875
Bryan Maass <bryan.maass@aclaimant.com>
2021-04-15 13:45:03 -0500

    update scratch with some more stuff

--- scratch.clj
+++ scratch.clj
@@ -6,10 +6,28 @@
 
 (def input [1721 979 366 299 675 1456])
 
-;; > (clojure.pprint/pprint 
-;;    (for [r (range 10)] 
-;;      (range (rand-int (* 10 r)))))
+(pprint/pprint
+ (for [r (range 10)]
+   (range (rand-int (* 2 r)))))
+
+
+(def a 1)
+
+(square a)
+
+;; fib sequence:
+
+(defn fib [n]
+  (map first
+       (take n
+             (iterate (fn [[a b]] [(+ a b) b]) [0 1]))))
+
+> (fib 20)
+
+> (map
+   (comp read-string #(apply str %) reverse str)
+   (fib 20))
 
 ;;--;;
 
-> (def a 1)
+(println "below fold, doesn't get executed.")
